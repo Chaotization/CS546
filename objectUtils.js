052070b3db5199d576727385f9de676badf2bd1d
Chaotization <103400111+Chaotization@users.noreply.github.com>
2023-02-10 19:48:31 -0500

    Add files via upload

--- objectUtils.js
+++ objectUtils.js
@@ -2,14 +2,26 @@
       using the ES6 exports syntax. 
       DO NOT CHANGE THE FUNCTION NAMES
 */
-
-let areObjectsEqual = (...args) => {
+function checkObjectsCondition(...args){
+      if(typeof args !== "object" || !args){
+            throw "Error: check that input exists and is of proper type"
+      }
+      if(args.length < 2){
+            throw "Error: there are at least two objects passed into the function"
+      }
+}
+export let areObjectsEqual = (...args) => {
       //this function takes in a variable number of objects that's what the ...args signifies
+      checkObjectsCondition(...args);
+      console.log(args.keys());
+      for(let i = 0; i < args.length; i++){
+            
+      }
       
 };
 
-let calculateObject = (object, funcs) => {};
+export let calculateObject = (object, funcs) => {};
 
-let combineObjects = (...args) => {
+export let combineObjects = (...args) => {
       //this function takes in a variable number of objects that's what the ...args signifies
 };
